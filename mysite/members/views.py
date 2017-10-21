b705d95077c580dee8c68a4b822856f0c163f5d5
Ver Starr <ver.starr@gmail.com>
2017-10-21 14:08:19 -0700

    started working on wiring models to views

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Chapter, Member
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'members/index.html'
+    context_object_name = 'latest_question_list'
+
+
+
+
+class DetailView(generic.DetailView):
+    model = Member
+    template_name = 'members/detail.html'
